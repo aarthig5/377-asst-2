77dd52d215542c81c0c5c2ed5836b68036b9aec7
aarthiganesh55 <61809010+aarthiganesh55@users.noreply.github.com>
2020-11-04 20:55:36 -0500

    script updates

--- public/script.js
+++ public/script.js
@@ -1,3 +1,25 @@
+function convertToCat(nameList) {
+    // process your restaurants here!
+    const nameLi = [];
+    const nameObj = {};
+    var i;
+    for (let i=0; i<nameList.length; i++) {
+        nameLi.push(nameList[i].category);
+      if (!nameObj[nameLi[i]]) {
+        nameObj[nameLi[i]] = 0;
+      }
+      nameObj[nameLi[i]] += 1;
+      // console.log(restaurantList[i].category);
+    }
+    //   console.log(catObj);
+    const list = Object.keys(nameObj).map((m) => ({
+      y: nameObj[m],
+      label: m
+    }));
+  
+    return list;
+  }
+
 
 function getRestaurantArr(jsonFromServer){
     console.log('jsonFromServer',jsonFromServer);
